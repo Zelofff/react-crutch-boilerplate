b240214472ab7e34bc9b977966804c66d0cb3321
zeloff <zelofflab@yandex.ru>
2019-03-20 12:28:47 +0300

    :wrench: Add webpack & babel

---
+++ config/webpack.base.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
