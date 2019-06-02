001ef4cc6843ce143409bf46653aa05f84b3532f
zelofff <zelofflab@yandex.ru>
2019-06-02 23:47:29 +0300

    :arrow_up: Update outdated dependecies

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { App } from './app'
-import { render, cleanup } from 'react-testing-library'
+import { render, cleanup } from '@testing-library/react'
 import 'jest-dom/extend-expect'
 
 afterEach(cleanup)
