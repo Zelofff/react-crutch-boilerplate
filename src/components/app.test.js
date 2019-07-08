91af0c6652912c26148c1296dcf8a4933b5f6d5d
zelofff <zelofflab@yandex.ru>
2019-07-09 01:29:22 +0300

    :arrow_up: Upgraded dependencies

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { App } from './app'
 import { render, cleanup } from '@testing-library/react'
-import 'jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect'
 
 afterEach(cleanup)
 
