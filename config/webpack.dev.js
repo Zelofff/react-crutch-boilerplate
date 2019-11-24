42cd84802972c7169e43da0ef793e27b2029467c
zelofff <zelofflab@yandex.ru>
2019-11-24 20:09:57 +0300

    :sparkles: change source map

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge')
 
 module.exports = merge(base, {
   mode: 'development',
-  devtool: 'eval-source-map',
+  devtool: 'cheap-module-eval-source-map',
   devServer: {
     port: 8000
   }
